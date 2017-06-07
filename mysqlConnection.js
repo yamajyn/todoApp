9a2e7418829fb91ccc485fc28145cb54cbc49db6
yamaj <yamamotojunya@team-lab.com>
2017-06-07 11:46:08 +0900

    deploy

--- mysqlConnection.js
+++ mysqlConnection.js
@@ -1,26 +1,26 @@
 var mysql = require('mysql');
 
 //ローカル用
-var dbConfig = {
-  host: '127.0.0.1',
-  user: 'root',
-  password: '',
-  database: 'todo_list'
-};
+// var dbConfig = {
+//   host: '127.0.0.1',
+//   user: 'root',
+//   password: '',
+//   database: 'todo_list'
+// };
 
 //リリース用
-// var dbConfig = {
-//   host: 'us-cdbr-iron-east-03.cleardb.net',
-//   user: 'ba09dd9960344c',
-//   password: '3f67f472',
-//   database: 'heroku_4d5fb8a6ef36520'
-// };
+var dbConfig = {
+  host: 'us-cdbr-iron-east-03.cleardb.net',
+  user: 'ba09dd9960344c',
+  password: '3f67f472',
+  database: 'heroku_4d5fb8a6ef36520'
+};
 
 var connection = mysql.createConnection(dbConfig);
 
 //HerokuのMySQLのためのハック
-// setInterval(function() {
-//   connection.query('SELECT 1');
-// }, 5000);
+setInterval(function() {
+  connection.query('SELECT 1');
+}, 5000);
 
 module.exports = connection;
