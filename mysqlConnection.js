c4671fc48133e35a3a665d05a035fd369d0d831a
yamaj <yamamotojunya@team-lab.com>
2017-06-08 19:17:52 +0900

    環境で接続先を変更するようにした

--- mysqlConnection.js
+++ mysqlConnection.js
@@ -1,26 +1,28 @@
 var mysql = require('mysql');
+var connection;
 
-//ローカル用
-var dbConfig = {
-  host: '127.0.0.1',
-  user: 'root',
-  password: '',
-  database: 'todo_list'
-};
-
-//リリース用
-// var dbConfig = {
-//   host: 'us-cdbr-iron-east-03.cleardb.net',
-//   user: 'ba09dd9960344c',
-//   password: '3f67f472',
-//   database: 'heroku_4d5fb8a6ef36520'
-// };
-
-var connection = mysql.createConnection(dbConfig);
-
-//HerokuのMySQLのためのハック
-// setInterval(function() {
-//   connection.query('SELECT 1');
-// }, 5000);
+if(process.env.NODE_ENV === "production" ){
+  //リリース用
+  var dbConfig = {
+    host: 'us-cdbr-iron-east-03.cleardb.net',
+    user: 'ba09dd9960344c',
+    password: '3f67f472',
+    database: 'heroku_4d5fb8a6ef36520'
+  };
+  connection = mysql.createConnection(dbConfig);
+  //HerokuのMySQLのためのハック
+  setInterval(function() {
+    connection.query('SELECT 1');
+  }, 5000);
+}else{
+  //ローカル用
+  var dbConfig = {
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'todo_list'
+  };
+  connection = mysql.createConnection(dbConfig);
+}
 
 module.exports = connection;
